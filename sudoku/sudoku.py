eb36d053b5ddf054c745d97234e7566b5357185e
Rand <ced.br@me.com>
2018-11-30 23:41:45 +0100

    Fini le constructeur de la classe Sudoku

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,16 +1,29 @@
 import math
+from typing import List
+
 
 class Sudoku:
     """Classe définissant une grille de sudoku et les méthodes necessaire à sa résolution"""
-
+    _board_array: List[List[int]]
+    _size: int
+    _board: str
 
     def __init__(self, board):
         """
 
         :type size: Taille de la grille sudoku
         """
-        self.board = board
+        self._board = board
+        self._size = int(math.sqrt(len(self._board)))
 
+        self._board_array = []
+        array: List[int] = []
+
+        for i, num in enumerate(self._board):
+            array.append(int(num))
+            if (i + 1) % self._size == 0 and i != 0:
+                self._board_array.append(array)
+                array = []
 
     def is_in_row(self, row: int, number: int):
         """Vérifie si la ligne contient déjà le nombre number.
@@ -46,15 +59,15 @@ class Sudoku:
 
     def display(self):
         """Affiche la grille sur la console."""
-        #print(self.board)
-        size = len(self.board)
+        #print(self._board)
+        size = len(self._board)
         size_box = math.sqrt(math.sqrt(size))
-        #print(size_box)
-        print_row_separator = " " + ("-" * int((size / (size_box * (size_box + 1)/size_box))))
-        #print(len(print_row_separator))
+        # print(size_box)
+        print_row_separator = " " + ("-" * int((size / (size_box * (size_box + 1) / size_box))))
+        # print(len(print_row_separator))
         print_row = '| '
         print(print_row_separator)
-        for i, row in enumerate(self.board):
+        for i, row in enumerate(self._board):
             print_row += '{0} '.format(row)
             if (i + 1) % math.sqrt(math.sqrt(size)) == 0:
                 print_row += '| '
@@ -63,4 +76,3 @@ class Sudoku:
                 print_row = '| '
             if (i + 1) % (size / size_box) == 0:
                 print(print_row_separator)
-
